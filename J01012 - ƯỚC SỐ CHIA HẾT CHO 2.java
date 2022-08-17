1560960e1223998a02ba4f34a5e08c819e1a6fa2
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-17 17:30:05 +0700

    Create J01012 - ƯỚC SỐ CHIA HẾT CHO 2.java

---
+++ J01012 - ƯỚC SỐ CHIA HẾT CHO 2.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        t = sc.nextInt();
+        while (t-->0) {
+            int n = sc.nextInt();
+            int cnt = 0;
+            for (int i = 1; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    if (i % 2 == 0) cnt++;
+                    if (n/i % 2 == 0) cnt++;
+                    if (i * i == n && i % 2 == 0) cnt--;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+}
+
