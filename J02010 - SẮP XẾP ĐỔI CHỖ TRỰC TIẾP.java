a3da99c4897385099d0675dbce921c2e1b48839f
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-20 07:05:52 +0700

    Create J02010 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.java

---
+++ J02010 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.java
@@ -0,0 +1,39 @@
+
+package newclass;
+
+import java.util.*;
+
+public class NewClass {
+    
+    public static void swap (int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int t = 1;
+       //t = sc.nextInt();
+       while (t--> 0) {
+           int n = sc.nextInt();
+           int[] a = new int[n];
+           for (int i = 0; i < n; i++) {
+               a[i] = sc.nextInt();
+           }
+           for (int i = 0; i < n - 1; i++) {
+               System.out.print("Buoc " + (i + 1) + ": ");
+               for (int j = i + 1; j < n; j++) {
+                   if (a[j] < a[i]) {
+                       swap(a, i, j);
+                   }
+               }
+               for (Integer x : a) {
+                   System.out.print(x + " ");
+               }
+               System.out.println("");
+           }
+       }
+    }
+    
+}
