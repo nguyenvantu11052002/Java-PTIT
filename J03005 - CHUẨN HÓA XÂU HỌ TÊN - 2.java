cb560f2085f6a54a3189809fcbb54809026e2ee4
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-21 10:05:25 +0700

    Create J03005 - CHUẨN HÓA XÂU HỌ TÊN - 2.java

---
+++ J03005 - CHUẨN HÓA XÂU HỌ TÊN - 2.java
@@ -0,0 +1,37 @@
+package newclass;
+
+import java.util.*;
+
+public class NewClass {
+
+    public static String solve(String s) {
+        s = s.trim();
+        String[] arr = s.split("\\s+");
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 1; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)));
+            for (int j = 1; j < arr[i].length(); j++) {
+                sb.append(Character.toLowerCase(arr[i].charAt(j)));
+            }
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(", ");
+        for (int i = 0; i < arr[0].length(); i++) {
+            sb.append(Character.toUpperCase(arr[0].charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            System.out.println(solve(s));
+        }
+    }
+}
+
+
