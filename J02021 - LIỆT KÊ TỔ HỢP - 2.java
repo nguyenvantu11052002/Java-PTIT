d0d896f2472b4cda5b01115e86c68f0bbf0fe38c
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-21 08:00:58 +0700

    Create J02021 - LIỆT KÊ TỔ HỢP - 2.java

---
+++ J02021 - LIỆT KÊ TỔ HỢP - 2.java
@@ -0,0 +1,39 @@
+package newclass;
+
+import java.util.*;
+
+public class NewClass {
+    public static int n, k, cnt;
+    public static int[] a = new int[15];
+    
+    public static void solve () {
+        for (int i = 1; i <= k; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.print(" ");
+    }
+    
+    public static void Try (int i) {
+        for (int j = a[i - 1] + 1; j <= n - k + i; j++) {
+            a[i] = j;
+            if (i == k) {
+                solve();
+                cnt++;
+            }
+            else Try(i + 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        //t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            n = sc.nextInt(); k = sc.nextInt();
+            cnt = 0;
+            Try(1);
+            System.out.println("");
+            System.out.println("Tong cong co " + cnt + " to hop");
+        }
+    }
+}
