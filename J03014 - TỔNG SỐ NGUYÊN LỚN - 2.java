0bce7831c8bba2fd1de5501a6f0478f7dce8a54a
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-29 09:47:15 +0700

    Create J03014 - TỔNG SỐ NGUYÊN LỚN - 2.java

---
+++ J03014 - TỔNG SỐ NGUYÊN LỚN - 2.java
@@ -0,0 +1,27 @@
+package javaptit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JavaPTIT {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        //t = Integer.parseInt(sc.nextLine());
+        while (t-->0) {
+            BigInteger a = new BigInteger(sc.nextLine());
+            BigInteger b = new BigInteger(sc.nextLine());
+            System.out.println(a.add(b));
+        }
+    }
+}
+
+//1
+//12
+//198111
