fa5f1b464b35e637b89e5862b8afea518dbdf7d6
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-08-31 20:17:39 +0700

    Create J05003 - DANH SÁCH ĐỐI TƯỢNG SINH VIÊN - 1.java

---
+++ J05003 - DANH SÁCH ĐỐI TƯỢNG SINH VIÊN - 1.java
@@ -0,0 +1,50 @@
+package javaptit;
+
+import java.util.*;
+
+public class SinhVien {
+
+    private String maSinhVien, hoTen, lop, ngaySinh;
+    private double gpa;
+    public static int id = 1;
+
+    public SinhVien(String hoTen, String lop, String ngaySinh, double gpa) {
+        this.maSinhVien = "B20DCCN" + String.format("%03d", id++);
+        this.hoTen = hoTen;
+        this.lop = lop;
+        this.ngaySinh = ngaySinh;
+        this.gpa = gpa;
+    }
+
+    public void chuanHoa() {
+        String[] arr = this.ngaySinh.trim().split("/");
+        if (arr[0].length() < 2) {
+            arr[0] = "0" + arr[0];
+        }
+        if (arr[1].length() < 2) {
+            arr[1] = "0" + arr[1];
+        }
+        this.ngaySinh = arr[0] + "/" + arr[1] + "/" + arr[2];
+    }
+
+    @Override
+    public String toString() {
+        return this.maSinhVien + " " + this.hoTen + " " + this.lop + " " + this.ngaySinh + " " + String.format("%.2f", this.gpa);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        //t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(sc.nextLine());
+            for (int i = 0; i < n; i++) {
+                String ten = sc.nextLine(), lop = sc.nextLine(), ngaySinh = sc.nextLine();
+                double gpa = Double.parseDouble(sc.nextLine());
+                SinhVien x = new SinhVien(ten, lop, ngaySinh, gpa);
+                x.chuanHoa();
+                System.out.println(x);
+            }
+        }
+    }
+}
