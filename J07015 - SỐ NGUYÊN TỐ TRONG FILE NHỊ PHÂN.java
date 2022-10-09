d7b549874c0a75205f7454dac3691b9011392234
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 21:03:44 +0700

    Update J07015 - SỐ NGUYÊN TỐ TRONG FILE NHỊ PHÂN.java

--- J07015 - SỐ NGUYÊN TỐ TRONG FILE NHỊ PHÂN.java
+++ J07015 - SỐ NGUYÊN TỐ TRONG FILE NHỊ PHÂN.java
@@ -1,32 +1,48 @@
+javaptit/CacHam.java
+
 package javaptit;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.*;
 
-public class Main {
-
-    public static int[] F = new int[1000005];
-
-    public static void sangnto() {
+public class CacHam {
+    public static long[] F = new long[1000005];
+    
+    public static void sangnto () {
         Arrays.fill(F, 1);
         F[0] = F[1] = 0;
         for (int i = 2; i <= 1001; i++) {
-            if (F[i] != 0) {
+            if (F[i] == 1) {
                 for (int j = i * i; j <= 1000001; j += i) {
                     F[j] = 0;
                 }
             }
         }
     }
+}
 
-    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+javaptit/Main.java
+package javaptit;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.util.*;
+import static javaptit.CacHam.F;
+import static javaptit.CacHam.sangnto;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         sangnto();
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream("SONGUYEN.in"));
-        List<Integer> list = (List<Integer>) ois.readObject();
+        List<Integer> list = new ArrayList<>();
+        list = (List<Integer>) ois.readObject();
         Map<Integer, Integer> map = new TreeMap<>();
         for (Integer x : list) {
             if (F[x] == 1) {
@@ -41,4 +57,6 @@ public class Main {
             System.out.println(x.getKey() + " " + x.getValue());
         }
     }
+
 }
+
