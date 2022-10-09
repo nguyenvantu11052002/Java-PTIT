da4185a60a79efde8b770080f65d970dac59c380
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 20:37:13 +0700

    Update J07005 - SỐ KHÁC NHAU TRONG FILE - 2.java

--- J07005 - SỐ KHÁC NHAU TRONG FILE - 2.java
+++ J07005 - SỐ KHÁC NHAU TRONG FILE - 2.java
@@ -1,3 +1,4 @@
+
 package javaptit;
 
 import java.io.DataInputStream;
@@ -5,27 +6,25 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class JavaPTIT {
 
-    public static void main(String[] args) throws IOException {
-        try {
-            DataInputStream dt = new DataInputStream(new FileInputStream("DATA.IN"));
-            Map<Integer, Integer> map = new TreeMap<>();
-            for (int i = 0; i < 100000; i++) {
-                int x = dt.readInt();
-                if (map.containsKey(x)) {
-                    map.put(x, map.get(x) + 1);
-                }
-                else map.put(x, 1);
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        DataInputStream dis = new DataInputStream(new FileInputStream("DATA.IN"));
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < 100000; i++) {
+            int n = dis.readInt();
+            if (map.containsKey(n)) {
+                map.put(n, map.get(n) + 1);
             }
-            for (Map.Entry<Integer, Integer> x : map.entrySet()) {
-                System.out.println(x.getKey() + " " + x.getValue());
-            }
-            dt.close();
-        } catch (Exception e) {
-
+            else map.put(n, 1);
         }
+        for (Map.Entry<Integer, Integer> x : map.entrySet()) {
+            System.out.println(x.getKey() + " " + x.getValue());
+        }
+        dis.close();
     }
+    
 }
