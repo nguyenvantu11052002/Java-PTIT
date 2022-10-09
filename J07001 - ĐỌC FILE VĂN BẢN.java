39be5c32dd146c09f356d3f2e1353ddb75cd0b1a
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 19:33:03 +0700

    Update J07001 - ĐỌC FILE VĂN BẢN.java

--- J07001 - ĐỌC FILE VĂN BẢN.java
+++ J07001 - ĐỌC FILE VĂN BẢN.java
@@ -1,47 +1,20 @@
+
 package javaptit;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.math.*;
+import java.io.PrintWriter;
 import java.util.*;
 
-public class JavaPTIT1 {
-      
+public class JavaPTIT {
+
     public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner sc = new Scanner(System.in);
-
-        int t = 1;
-        //t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            try {
-                FileWriter fw = new FileWriter("DATA.in");
-                BufferedWriter bw = new BufferedWriter(fw);
-                String line = "";
-                while (true) {
-                     line = sc.nextLine();
-                     bw.write(line);
-                }
-            } catch (Exception e) {
-                
-            }
-            
-            try {
-                FileReader fr = new FileReader("DATA.in");
-                BufferedReader br = new BufferedReader(fr);
-                String line = "";
-                while (true) {
-                     line = br.readLine();
-                     if (line.equals(null)) break;
-                     System.out.println(line);
-                }
-            } catch (Exception e) {
-                
-            }
+        Scanner sc = new Scanner(new File("DATA.in"));
+        //PrintWriter out = new PrintWriter(new File("DATAOUT.txt"));
+        while(sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
         }
+        sc.close();
     }
+    
 }
-
