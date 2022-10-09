f97cd982e77536575ccee9b884c0f96c9ae0e6bf
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 20:04:40 +0700

    Update J07004 - SỐ KHÁC NHAU TRONG FILE - 1.java

--- J07004 - SỐ KHÁC NHAU TRONG FILE - 1.java
+++ J07004 - SỐ KHÁC NHAU TRONG FILE - 1.java
@@ -1,30 +1,27 @@
+
 package javaptit;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class JavaPTIT {
 
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("DATA.in"));
         Map<Integer, Integer> map = new TreeMap<>();
-        
-        try {
-            File file = new File("DATA.in");
-            Scanner sc = new Scanner(file);
-            while (sc.hasNext()) {
-                int n = sc.nextInt();
-                if (map.containsKey(n)) {
-                    map.put(n, map.get(n) + 1);
-                }
-                else map.put(n, 1);
+        while(sc.hasNext()) {
+            int n = Integer.parseInt(sc.next());
+            if (map.containsKey(n)) {
+                map.put(n, map.get(n) + 1);
             }
-            for (Map.Entry<Integer, Integer> x : map.entrySet()) {
-                System.out.println(x.getKey() + " " + x.getValue());
-            }
-        } catch (Exception e) {
-
+            else map.put(n, 1);
         }
+        for (Map.Entry<Integer, Integer> x : map.entrySet()) {
+            System.out.println(x.getKey() + " " + x.getValue());
+        }
+        sc.close();
     }
-
+    
 }
