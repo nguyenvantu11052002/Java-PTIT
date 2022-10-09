82fa640cc5e07d2b09b77e4676ee5e0644eda8ba
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-09 20:48:05 +0700

    Update J07007 - LIỆT KÊ TỪ KHÁC NHAU.java

--- J07007 - LIỆT KÊ TỪ KHÁC NHAU.java
+++ J07007 - LIỆT KÊ TỪ KHÁC NHAU.java
@@ -1,39 +1,55 @@
+javaptit/JavaPTIT.java
+
+package javaptit;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class JavaPTIT {
+
+    public static void main(String[] args) throws IOException {
+        WordSet ws = new WordSet("VANBAN.in");
+        System.out.println(ws);
+    }
+    public static void main8956705(String[] args) throws IOException {
+        WordSet ws = new WordSet("VANBAN.in");
+        System.out.println(ws);
+    }
+    
+}
+
+
+javaptit/WordSet.java
+
 package javaptit;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.*;
 
 public class WordSet {
-    private Set<String> set = new TreeSet<>();
-   
-    public WordSet (String ss) throws FileNotFoundException  {
-        Scanner sc = new Scanner(new File("VANBAN.in"));
-        String s = "";
+    Set<String> set = new TreeSet<>();
+    public WordSet(String s) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(s));
         while (sc.hasNext()) {
-            s = sc.next();
-            s = s.toLowerCase();
-            this.set.add(s);
+            String tmp = sc.next().toLowerCase();
+            set.add(tmp);
         }
     }
     
     @Override
-    public String toString () {
+    public String toString() {
         String res = "";
         for (String x : this.set) {
             res += x + "\n";
         }
         return res;
     }
-    
-    public static void main(String[] args) throws IOException {
-        WordSet ws = new WordSet("VANBAN.in");
-        System.out.println(ws);
-    }
-    public static void main9629608(String[] args) throws IOException {
-        WordSet ws = new WordSet("VANBAN.in");
-        System.out.println(ws);
-    }
-    
 }
+
+
