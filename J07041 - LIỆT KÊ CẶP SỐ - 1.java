189b2d833f468fd5678d8f1f12ee3e1f4c937bc3
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-10-22 18:02:38 +0700

    Create J07041 - LIỆT KÊ CẶP SỐ - 1.java

---
+++ J07041 - LIỆT KÊ CẶP SỐ - 1.java
@@ -0,0 +1,71 @@
+test/Main.java
+
+package test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DATA.in"));
+        List<Pair> list = (List<Pair>) ois.readObject();
+        Map<Pair, Integer> map = new HashMap<>();
+        List<Pair> res = new ArrayList<>();
+        for (Pair x : list) {
+            if (x.getFirst() < x.getSecond()) {
+                if (map.containsKey(x)) {
+                    
+                }
+                else {
+                    map.put(x, 1);
+                    res.add(x);
+                }
+            }
+        }
+        Collections.sort(res);
+        for (Pair x : res) {
+            System.out.println(x);
+        }
+    }
+    
+}
+
+test/Pair.java
+
+package test;
+
+import java.io.Serializable;
+
+public class Pair implements Serializable, Comparable<Pair> {
+    private int first, second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    public int getFirst() {
+        return first;
+    }
+    public int getSecond(){
+        return second;
+    }
+    @Override
+    public int compareTo(Pair o) {
+        return this.first - o.first;
+    }
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+    
+}
+
