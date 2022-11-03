fb08858d2acfd4e0fa7bf54af56c0089e094c82b
Nguyễn Văn Tú <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-11-03 14:18:28 +0700

    Create J04020 - LỚP PAIR.java

---
+++ J04020 - LỚP PAIR.java
@@ -0,0 +1,89 @@
+Main.java
+package javaptit;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            boolean check = false;
+            for(int i = 2; i <= 2*Math.sqrt(n); i++){
+                Pair<Integer, Integer> p = new Pair<>(i, n-i);
+                if(p.isPrime()){
+                    System.out.println(p);
+                    check = true;
+                    break; 
+                }
+            }
+            if(!check) System.out.println(-1);
+        }
+    }
+    public static void main9837762(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            boolean check = false;
+            for(int i = 2; i <= 2*Math.sqrt(n); i++){
+                Pair<Integer, Integer> p = new Pair<>(i, n-i);
+                if(p.isPrime()){
+                    System.out.println(p);
+                    check = true;
+                    break; 
+                }
+            }
+            if(!check) System.out.println(-1);
+        }
+    }
+}
+
+Pair.java
+package javaptit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    public boolean nto (int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    public boolean isPrime () {
+        if (nto((Integer) this.first) && nto((Integer) this.second)) return true;
+        return false;
+    }
+    
+    @Override
+    public String toString () {
+        return this.first + " " + this.second;
+    }
+
+}
+
